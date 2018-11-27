d38c953f93056cb7ff8e3dd0e0999e8bf4590190
Matthew Nielsen <matthewnielsen27@gmail.com>
2018-11-27 14:17:27 -0500

    update mac_app_store

--- straps/mac_app_store/mac_app_store.sh
+++ straps/mac_app_store/mac_app_store.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-
-strapped_mac_app_store_before () { 
-    if ! mas version > /dev/null; then echo "🍏 mas is missing" && exit; fi 
+strapped_mac_app_store_before () {
+    if ! mas version > /dev/null; then echo "🍏 mas is missing" && exit; fi
 }
 
 strapped_mac_app_store () {
     local mas_count
-    local user_json
+    local user_config=$1
+    local name
+    local id
 
-    user_json=$1
-    mas_count=$(jq -r '.apps | length' <<< "$user_json")
+    mas_count=$(q_count "$user_config" "apps.\\[[0-9]+\\].name")
 
     for (( i=0; i < mas_count; i++ )); do
-        name=$(jq -r ".apps[${i}].name" <<< "$user_json")
-        id=$(jq -r ".apps[${i}].id" <<< "$user_json")
+        name=$(q "$user_config" "apps.\\[${i}\\].name")
+        id=$(q "$user_config" "apps.\\[${i}\\].id")
         echo "🍏 installing ${name}"
         mas install "${id}"
     done
 }
 
-strapped_mac_app_store_after () { 
-  return   
+strapped_mac_app_store_after () {
+  return
 }
